f775af6111395317e6f7499572db304afccf6ad1
hadarYeger13 <hadarygr@gmail.com>
2024-10-13 02:34:05 +0300

    Update home lock

--- src/pages/HomeLock.tsx
+++ src/pages/HomeLock.tsx
@@ -1,12 +1,10 @@
 import React, { FC, useState } from "react";
-import { Box, Button, Divider, IconButton, SxProps, TextField, Typography } from "@mui/material";
-import { Add, Delete } from "@mui/icons-material";
+import { Box, Button, TextField } from "@mui/material";
 import { useWindowSize } from "react-use";
 import ReactConfetti from "react-confetti";
 
 export const HomeLock: FC = () => {
-    const [newValue, setNewValue] = useState<string>("");
-    const [values, setValues] = useState<string[]>([]);
+    const [value, setValue] = useState<string>("");
     const [error, setError] = useState<boolean>(false);
 
     const { width, height } = useWindowSize();
@@ -14,82 +12,33 @@ export const HomeLock: FC = () => {
 
     const onValueChange = (e) => {
         setError(false);
-        setNewValue(e.target.value)
-
-    }
-
-    const onAddClick = () => {
-        if (newValue.trim() !== "") {
-            setValues(prevValues => [...prevValues, newValue]);
-            setNewValue("");
-        }
-    }
-
-    const onDeleteClick = (index: number) => {
-        setError(false);
-        setValues(prevValues => {
-            prevValues.splice(index, 1);
-            return [...prevValues];
-        })
+        setValue(e.target.value);
     }
 
     const onSubmit = () => {
-        if (values.length !== 9) {
-            setError(true);
-        }
-        else if (!values.includes("עיניים") || !values.includes("גבות") || !values.includes("סנטר") || !values.includes("לחיים") || !values.includes("אף") || !values.includes("נחיריים")
-            || !values.includes("מצח") || !values.includes("פה") || !values.includes("רקות")) {
-            setError(true);
+        if (value === "") {
             return;
         }
-        else {
+        else if (value === "סלמנדרה") {
             setShowConfetti(true);
+        }
+        else {
+            setError(true);
         }
     };
 
     return (
         <>
-            <Box sx={containerSxProps}>
-                <Box sx={{ display: "flex", flexDirection: "row", gap: "8px", alignItems: "center" }}>
-                    <Button
-                        startIcon={<Add/>}
-                        onClick={onAddClick}
-                    >
-                        Add
-                    </Button>
-                    <TextField
-                        dir={"rtl"}
-                        value={newValue}
-                        onChange={onValueChange}
-                        error={error}
-                    />
-                </Box>
-                <Box sx={{ flexGrow: 1, overflowY: "auto" }}
+            <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "24px", marginTop: "24px" }}>
+                <TextField
                     dir={"rtl"}
-                >
-                    {values.map((value, index) => (
-                        <Box
-                            key={index}
-                        >
-                            <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between", width: "273px" }}>
-                                <Typography sx={{ overflow: "hidden", textOverflow: "ellipsis" }}
-                                >
-                                    {value}
-                                </Typography>
-                                <IconButton
-                                    onClick={() => onDeleteClick(index)}
-                                >
-                                    <Delete/>
-                                </IconButton>
-                            </Box>
-                            <Divider/>
-                        </Box>
-
-                    ))}
-                </Box>
-                <Button
-                    variant={"contained"}
-                    onClick={onSubmit}
+                    value={value}
+                    onChange={onValueChange}
+                    error={error}
+                />
+                <Button sx={{ width: "fit-content" }}
+                        variant={"contained"}
+                        onClick={onSubmit}
                 >
                     Submit
                 </Button>
@@ -104,15 +53,4 @@ export const HomeLock: FC = () => {
             }
         </>
     )
-}
-
-const containerSxProps: SxProps = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    gap: "24px",
-    paddingTop: "24px",
-    paddingBottom: "24px",
-    maxHeight: "calc(100vh - 16px)", // I need -16px due to the body margin
-    boxSizing: "border-box"
 }
